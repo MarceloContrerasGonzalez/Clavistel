c4488dc1a938b2303c2ccff76ff15625e5539a99
MarceloContrerasGonzalez <marc.contreras@duocuc.cl>
2022-04-15 18:07:39 -0400

    validacion login

--- js/validacion.js
+++ js/validacion.js
@@ -1,20 +1,38 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+/* VALIDACION JQUERY */
+
+/* USUARIO  (probando)*/
+
+$(document).ready(function () {
+  $("#bRegistro").click(function () {
+    var usuario = $("#inputUsuario").val();
+
+    if (usuario == "") {
+      $("#errorUser").fadeIn();
+      return false;
+    } else {
+      $("#errorUser").hide();
+    }
+
+  })
+}) 

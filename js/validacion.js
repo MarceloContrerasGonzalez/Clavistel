93bb2ff756c6c7f0fac7f274d5b4ceb398c4e786
MarceloContrerasGonzalez <marc.contreras@duocuc.cl>
2022-04-17 21:49:35 -0400

    arregle algunos errores del nuevo diseño de login que hizo mi compañero

--- js/validacion.js
+++ js/validacion.js
@@ -22,41 +22,20 @@
 
 /* VALIDACION JQUERY */
 
-
-/* Validar Usuario*/
-
+/*Validar Contraseñas Iguales */
 $(document).ready(function () {
-  $("#bRegistro").click(function (event) {
-    var usuario = $("#inputUsuario").val();
+    $("#bRegistro").click(function (event) {
+    var password1 = $("#inputPassword").val();
+    var password2 = $("#inputPassword2").val();
 
-    if (usuario == "") {
-      $("#inputUsuario").focus();
-      $("#error1").fadeIn();
-      $("#inputUsuario").css({ 'borderColor': '#fa1b1b' });
-      event.preventDefault();
-
-    } else {
-      $("#error1").hide();
-      $("#inputUsuario").css({ 'borderColor': '#008000' });
-    }
-
-  });
-});
-
-/*Validar Correo*/
-$(document).ready(function () {
-  $("#bRegistro").click(function (event) {
-    var expr = /^([a-zA-Z0-9_.+-])+@(([a-zA-Z0-9-])+).+([a-zA-Z0-9]{2,4})+$/;
-    var correo = $("#inputCorreo").val();
-
-    if (correo == "" || !expr.test(correo)) {
-      $("#inputCorreo").focus();
-      $("#error2").fadeIn();
-      $("#inputCorreo").css({ 'borderColor': '#fa1b1b' });
+    if (password2 == "" || password2 !== password1) {
+      $("#inputPassword2").focus();
+      $("#error4").fadeIn();
+      $("#inputPassword2").css({ 'borderColor': '#fa1b1b' });
       event.preventDefault();
     } else {
-      $("#error2").hide();
-      $("#inputCorreo").css({ 'borderColor': '#008000' });
+      $("#error4").hide();
+      $("#inputPassword2").css({ 'borderColor': '#008000' });
     }
 
   });
@@ -80,22 +59,42 @@ $(document).ready(function () {
   });
 });
 
-/*Validar Contraseñas Iguales */
+
+/*Validar Correo*/
 $(document).ready(function () {
   $("#bRegistro").click(function (event) {
-    var password1 = $("#inputPassword").val();
-    var password2 = $("#inputPassword2").val();
+    var expr = /^([a-zA-Z0-9_.+-])+@(([a-zA-Z0-9-])+).+([a-zA-Z0-9]{2,4})+$/;
+    var correo = $("#inputCorreo").val();
 
-    if (password2 == "" || password2 !== password1) {
-      $("#inputPassword2").focus();
-      $("#error4").fadeIn();
-      $("#inputPassword2").css({ 'borderColor': '#fa1b1b' });
+    if (correo == "" || !expr.test(correo)) {
+      $("#inputCorreo").focus();
+      $("#error2").fadeIn();
+      $("#inputCorreo").css({ 'borderColor': '#fa1b1b' });
       event.preventDefault();
     } else {
-      $("#error4").hide();
-      $("#inputPassword2").css({ 'borderColor': '#008000' });
+      $("#error2").hide();
+      $("#inputCorreo").css({ 'borderColor': '#008000' });
     }
 
   });
 });
 
+/* Validar Usuario*/
+
+$(document).ready(function () {
+  $("#bRegistro").click(function (event) {
+    var usuario = $("#inputUsuario").val();
+
+    if (usuario == "") {
+      $("#inputUsuario").focus();
+      $("#error1").fadeIn();
+      $("#inputUsuario").css({ 'borderColor': '#fa1b1b' });
+      event.preventDefault();
+
+    } else {
+      $("#error1").hide();
+      $("#inputUsuario").css({ 'borderColor': '#008000' });
+    }
+
+  });
+});

6275bbb928fe62144641a7617fb26fd55dfe69c8
MarceloContrerasGonzalez <marc.contreras@duocuc.cl>
2022-05-11 18:35:32 -0400

    creacion de tablas sqlite

--- Telefonos/models.py
+++ Telefonos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+# TABLA Marca.
+class Marca(models.Model):
+    id_marca = models.IntegerField(primary_key=True,verbose_name='id_marca')
+    nom_marca = models.CharField(max_length=100,verbose_name='Marca')
+
+    def __str__(self):
+        return self.nom_marca
+    
+# TABLA TipoEnvio.
+class TipoEnvio(models.Model):
+    idTipoEnvio = models.IntegerField(primary_key=True,verbose_name='id_tipo_envio')
+    tipoEnvio = models.CharField(max_length=50,verbose_name='tipo_envio')
+
+    def __str__(self):
+        return self.tipoEnvio    
+
+# TABLA Movil.
+class Movil(models.Model):
+    id_movil = models.IntegerField(primary_key=True,verbose_name='id_movil')
+    img = models.ImageField(verbose_name='img')
+    costo = models.IntegerField(verbose_name='costo')
+    descripcion = models.CharField(max_length=100,verbose_name='descripcion')
+    id_marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    id_tipo_envio = models.ForeignKey(TipoEnvio, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.descripcion
+
+
+    

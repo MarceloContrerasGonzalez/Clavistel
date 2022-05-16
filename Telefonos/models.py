6e744cf12002f224040e1be241942a8ab5b1ab78
MarceloContrerasGonzalez <marc.contreras@duocuc.cl>
2022-05-16 19:41:11 -0400

    prueba imagen

--- Telefonos/models.py
+++ Telefonos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # TABLA Marca.
 class Marca(models.Model):
-    id_marca = models.IntegerField(primary_key=True,verbose_name='id_marca')
+    id_marca = models.IntegerField(primary_key=True,verbose_name='id marca')
     nom_marca = models.CharField(max_length=100,verbose_name='Marca')
 
     def __str__(self):
@@ -10,18 +10,18 @@ class Marca(models.Model):
     
 # TABLA TipoEnvio.
 class TipoEnvio(models.Model):
-    idTipoEnvio = models.IntegerField(primary_key=True,verbose_name='id_tipo_envio')
-    tipoEnvio = models.CharField(max_length=50,verbose_name='tipo_envio')
+    idTipoEnvio = models.IntegerField(primary_key=True,verbose_name='id envio')
+    tipoEnvio = models.CharField(max_length=50,verbose_name='tipo de envio')
 
     def __str__(self):
         return self.tipoEnvio    
 
 # TABLA Movil.
 class Movil(models.Model):
-    id_movil = models.IntegerField(primary_key=True,verbose_name='id_movil')
-    img = models.ImageField(upload_to='Telefonos/static/Telefonos/img/imgdjango',verbose_name='img')
+    id_movil = models.IntegerField(primary_key=True,verbose_name='id movil')
+    img = models.ImageField(upload_to='Telefonos/static/Telefonos/img/imgdjango',verbose_name='imagen')
     costo = models.IntegerField(verbose_name='precio')
-    descripcion = models.CharField(max_length=100,verbose_name='descripcion')
+    descripcion = models.CharField(max_length=100,verbose_name='nombre')
     id_marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     id_tipo_envio = models.ForeignKey(TipoEnvio, on_delete=models.CASCADE)
 
--- Telefonos/urls.py
+++ Telefonos/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import home, login, consulta, telefonos, registro
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns =[
     path('', home, name="home"),
@@ -8,3 +10,5 @@ urlpatterns =[
     path('telefonos', telefonos, name="telefonos"),
     path('registro', registro, name="registro")
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

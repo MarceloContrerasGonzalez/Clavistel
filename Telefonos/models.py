236c4e51ef5376975df7a38ddc9a372b9c87cfb3
HyrumBustosFernandez <hyrum.bustos@gmail.com>
2022-07-11 01:38:30 -0400

    Antes de cagarla

--- Telefonos/models.py
+++ Telefonos/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import datetime, timedelta
 
 # Tablas Telefonos
 
@@ -55,19 +55,29 @@ class Sucursal(models.Model):
 
 #Tablas Carrito
 
+
+def HoraPreparacion():
+    return datetime.today() + timedelta(hours=16)
+
+def HoraDespacho():
+    return datetime.today() + timedelta(hours=34)
+
+
 class Boleta(models.Model):
     num_boleta = models.AutoField(primary_key=True,verbose_name='Código Boleta')
     nom_cliente = models.CharField(max_length=50,verbose_name='Nombre Cliente')
     estado = models.IntegerField(default=0, verbose_name="Estado")
     fecha = models.DateTimeField(auto_now_add=True, verbose_name="Fecha")
+    fechaPreparacion = models.DateTimeField(default=HoraPreparacion, verbose_name="Fecha")
+    fechaDespacho = models.DateTimeField(default=HoraDespacho, verbose_name="Fecha")
     nom_productos = models.CharField(max_length=250,verbose_name='Productos')
     cant_total = models.IntegerField(verbose_name='Cantidad total')
-    cantidad = models.IntegerField(default = 5, verbose_name='Cantidades')
-    
+    cantidad = models.IntegerField(default=5, verbose_name='Cantidades')
     
     def __str__(self):
         return str(self.num_boleta)
     
+
 class Despacho(models.Model):
     id_despacho = models.AutoField(primary_key=True,verbose_name='Número Despacho')
     nom_destinatario = models.CharField(max_length=250,verbose_name='Nombre Destinatario')
